2ea8761a15769da6283e6bed2bf8ca258a4ee53c
Pravin Se <pvs1516@gmail.comm>
2022-08-10 10:58:29 +0100

    Update 11

---
+++ python_test_practice.py
@@ -0,0 +1,91 @@
+# create a function called user_name
+def user_name:
+    return
+
+# How would you iterate over the following list: arr = [1, 2, 3, 4, 5]?
+Answer = create a for loop
+
+# display the list with help of for loop and a print statement
+for x in arr:
+    print(x)
+
+# && and II andand which one is a correct boolean value/operator
+Answer = and
+
+# what is the diff between tuple and a list
+Answer = tuples cannot be changed but lists can be changed they are immutable
+
+# could you add an element in a tuple
+Answer = no
+
+# can you have multiple types of date in a tuple
+Answer = yes
+
+# daata_type = {"0": 0, "1!: 1, "2": 2,) what type of data dollection is this
+Answer = this is a dictionary
+
+# how to create an object of a class called DevOps():
+dev_ops = DevOps()
+object = dev_ops.object_name
+
+# adds 5 to the following list
+# arr = [1,2,3,4]
+arr.append(5)
+
+# using a function that takes 1 arg, return True if arg is equal to devops otherwise false
+def devops():
+    if argument == True
+        return "yes"
+    elif argument == False
+        return "no"
+
+# how to inherit a class - write the correct syntax if you had class Y and class Z - inherit class Z from class Y
+answer = Class_y(Class_z)
+# how to declare super
+answer = super()
+
+# # a function should return True if the value is contained in (list) at an even index, False otherwise
+def list_function(value, list):
+    counter = 0
+    for item in list:
+        if (value == item):
+            if (counter %2 == 0):
+                return True
+            elif:
+                return False
+
+
+
+
+
+# create 4 functions that take 2 args each percentage(Value1,value2), divide, calculate dob, multiply
+# in each function check to ensure the value entered is not 0
+# each function MUST RETURN correct mathematical value
+
+def multiply(a,b):
+    if(a != 0 and b != 0):
+        return a*b
+
+def divide(a,b):
+    if(a != 0 and b != 0):
+        return a/b
+
+def minus(a,b):
+    if(a != 0 and b != 0):
+        return a-b
+
+
+# searching for odd and even numbers using a function that an argument of ints
+# using range ()
+
+
+
+# create a function to calculate total value of shopping bill
+# shopping_list = {5 items with values 3.4,5.5,6, 7 in a key value format}
+# must RETURN the total amount only
+
+
+
+
+
+
